a1820e6ade0b0d0ccedc77851d11f9fa22dd383e
HardikGoyal2003 <hardikgoyal2003@gmail.com>
2023-11-05 23:27:25 +0530

    Day01

---
+++ Day01/README.md
@@ -0,0 +1,10 @@
+# Weather App
+
+Welcome to the Weather App repository! This application is designed to provide real-time weather information for locations around the world. With this app, users can check the current weather conditions, forecasts, and other relevant data for their desired city.
+
+## Features
+
+- **Current Weather**: Get up-to-date information on the current weather conditions for a specific location, including temperature, humidity, wind speed.
+
+- **Search by Location**: Easily search for weather information by entering the name of a city.
+
---
+++ Day01/script.js
@@ -0,0 +1,93 @@
+const apikey = "Your-API-Key";
+
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
+
+let search=document.querySelector(".search input")
+let search_btn=document.querySelector(".search button")
+let weather_icon=document.querySelector(".weather-icon")
+
+
+async function checkweather(){
+    try {
+
+        if(search.value==""){
+            const response= await fetch(apiUrl + "delhi" + `&appid=${apikey}`)
+            console.log("Fetching...")  
+            var data = await response.json();
+        }
+
+        else{
+            const response= await fetch(apiUrl + search.value+ `&appid=${apikey}`)
+            console.log("Fetching...")  
+            var data = await response.json();
+
+        }
+
+        console.log("Processing...")
+        console.log(data.name)
+
+        if(data.name!=undefined){
+            document.querySelector(".city").innerHTML=data.name
+            document.querySelector(".temp").innerHTML=Math.round(data.main.temp) + "°C"
+            document.querySelector(".humidity").innerHTML=data.main.humidity +"%"
+            document.querySelector(".wind").innerHTML=data.wind.speed + "km/h"
+        
+            let weather=data.weather[0].main
+            
+            if(weather=="Rain"){
+                weather_icon.src='images/rain.png'
+            }
+            
+            
+            else if(weather=="Smoke"){
+                weather_icon.src='images/mist.png'
+            }
+            
+            
+            else if(weather=="Clouds"){
+                weather_icon.src='images/clouds.png'
+            }
+            
+            else if(weather=="Clear"){
+                weather_icon.src='images/clear.png'
+            }
+            
+            else if(weather=="Atmosphere"){
+                weather_icon.src='images/mist.png'
+            }
+            
+            else if(weather=="Snow"){
+                weather_icon.src='images/snow.png'
+            }
+            
+            else if(weather=="Drizzle"){
+                weather_icon.src='images/drizzle.png'
+            }
+            else if(weather=="Thunderstorm"){
+                weather_icon.src='images/thunderstorm.png'
+            }
+            
+            
+            document.querySelector("#invalid").style.display='none'
+            console.log("done!")
+        }
+        else{
+
+            document.querySelector("#invalid").style.display='block'
+        }
+    } 
+    
+    catch(err){
+        
+        console.error(err)
+    }
+
+   
+
+
+}
+search_btn.addEventListener('click',()=>{
+    console.log("Running...")
+    checkweather()
+})
+
